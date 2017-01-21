d6406ea289a54ca80b505b9ecaaa443d5a8e8207
Josh Simonot <jpxgit@outlook.com>
2017-01-21 10:57:38 -0500

    Added license headers and small touchups

--- SlickLayout.java
+++ SlickLayout.java
@@ -1,27 +1,3 @@
-/*
- *	MIT License
- *
- *	Copyright (c) 2017 Josh Simonot
- *
- *	Permission is hereby granted, free of charge, to any person obtaining a copy
- *	of this software and associated documentation files (the "Software"), to deal
- *	in the Software without restriction, including without limitation the rights
- *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *	copies of the Software, and to permit persons to whom the Software is
- *	furnished to do so, subject to the following conditions:
- *	
- *	The above copyright notice and this permission notice shall be included in all
- *	copies or substantial portions of the Software.
- *	
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *	SOFTWARE.
- */
-
 package jpx.slick.layout;
 
 import java.awt.Component;
@@ -33,17 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Java Swing Layout Manager
- * 
- * Mix horizontal/vertical scaling components with fixed size 
- * components in a single layout. 
- * 
- * Lays components out in rows.
- * 
- * @author Josh
- *
- */
 public class SlickLayout implements LayoutManager2
 {
 	private List<List<Component>> componentListByRow = new ArrayList<>();
@@ -53,7 +18,6 @@ public class SlickLayout implements LayoutManager2
 	public void addLayoutComponent(String name, Component comp) 
 	{
 		//not supported
-		//throw exceptions??
 	}
 	
 	@Override
